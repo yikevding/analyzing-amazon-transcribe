d03c6e87b0aa1b27366c2d299d067514231ab8b1
YongqiZhang-1 <69965223+YongqiZhang-1@users.noreply.github.com>
2021-11-10 00:23:53 -0500

    Delete TranscribeSample.js

--- final-resources/TranscribeSample.js
+++
@@ -1,8 +0,0 @@
-{
-    "TranscriptionJobName": "Alexa-Naye-Text",
-    "LanguageCode": "en-US",
-    "Media": {
-        "MediaFileUri": "s3://final-resources/qtm350 recording — Naye.mp3"
-    },
-    "OutputBucketName": "final-resources"
-}
